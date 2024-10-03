16127a35851b91ad01e4727a4b5398e4c3720d0b
Alilopez12 <allisonlopez1928@gmail.com>
2024-10-02 20:37:34 -0500

    carpetas

---
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyectofinal/controller/UsuarioController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.proyectofinal.controller;
+
+public class UsuarioController {
+}
---
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyectofinal/viewcontroller/UsuarioViewController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.proyectofinal.viewcontroller;
+
+public class UsuarioViewController {
+}
